5d8e580f1775375a9f601475c604d9516f10f7a5
WashingtonYandun <washington.yandun06@gmail.com>
2022-03-23 14:49:45 -0500

    effect finished

--- js/script.js
+++ js/script.js
@@ -4,27 +4,27 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
 
 class Char {
-  constructor(x, y, canvasHeight) {
+  constructor(x, y, size, canvasHeight) {
+    this.symbols =
+      "W.;WashingtonYandunWMYM华盛顿wmym烟墩1234567890ヤンドゥン@#$€!?ワシントンWY";
     this.x = x;
     this.y = y;
-    this.canvasHeight = canvasHeight;
-    this.symbols =
-      "WashingtonYandunWMYM华盛顿wmym烟墩1234567890ヤンドゥン@#$€!?ワシントンWY";
+    this.size = size;
     this.current = "";
-    console.log(this.symbols[getRandomInt(0, this.symbols.length)]);
+    this.canvasHeight = canvasHeight;
   }
-  draw() {
-    this.current = this.symbols[getRandomInt(0, this.symbols.length)];
-    ctx.fillStyle = "#00ff41";
-    context.fillStyle = "#0aff0a";
-    context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.95) {
-      this.y = 0;
+  draw(ctx) {
+    this.current = this.symbols[getRandomInt(0, this.symbols.length - 1)];
+    ctx.fillStyle = "#22b455";
+
+    ctx.fillText(this.current, this.x * this.size, this.y * this.size);
+    if (this.y * (this.size + 1) > this.canvasHeight && Math.random() > 0.95) {
+      this.y = -1;
     } else {
       this.y += 1;
     }
@@ -35,30 +35,26 @@ class Matrix {
   constructor(canvasWidth, canvasHeight) {
     this.canvasWidth = canvasWidth;
     this.canvasHeight = canvasHeight;
-    this.fontSize = 25;
-    this.cols = parseInt(this.canvasWidth / this.fontSize);
-    this.symbols = [];
-    this.#init();
+    this.size = 15;
+    this.cols = parseInt(this.canvasWidth / this.size) + 1;
+    this.chars = [];
+    this.#initialize();
   }
-  #init() {
+  #initialize() {
     for (let i = 0; i < this.cols; i++) {
-      this.chars[i] = new Char(i, 0, this.fontSize, this.canvasHeight);
+      this.chars[i] = new Char(i, -1, this.size, this.canvasHeight);
     }
   }
 }
 
-const matrix = new Matrix(canvas.width, canvas.height);
+const mx = new Matrix(canvas.width, canvas.height);
 
 function animate() {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+  ctx.fillStyle = "rgba(0, 0, 0, 0.09)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.font = thisfontSize + "px monospace";
+  ctx.font = mx.size + "px 'Ubuntu Mono', monospace";
   ctx.fillStyle = "#0aff0a";
-
-  m.chars.forEach((c) => {
-    c.draw();
-  });
+  mx.chars.forEach((c) => c.draw(ctx));
   requestAnimationFrame(animate);
 }
-
 animate();
